961d5b1d4bd1889d9761d9b734efba8f9d324e87
DevPaul0 <itspaulogimenez@gmail.com>
2024-01-24 10:48:49 -0300

    i created the files structure using node.js + express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const port = 3030
+const path = require("path")
+
+createPath = (filename) => path.join(__dirname,`./views/${filename}.html`);
+app.use(express.static('public'))
+
+app.listen(port,() => console.log(`http://localhost:${port}`))
+
+app.get("/",(req,res) => res.sendFile(createPath("home")));
+
+app.get("/login",(req,res) => res.sendFile(createPath("login")));
+
+app.get("/productCart",(req,res) => res.sendFile(createPath("productCart")));
+
+app.get("/productDetail",(req,res) => res.sendFile(createPath("productDetail")));
+
+app.get("/register",(req,res) => res.sendFile(createPath("register")));
+
