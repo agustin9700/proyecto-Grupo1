e82b4d7ab681c42af36fd8c1e4826cd4909ea4b9
agustin9700 <ryuk10xd@gmail.com>
2024-01-29 22:24:49 -0300

    agregando arreglos en estructura

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require("path")
 createPath = (filename) => path.join(__dirname,`./views/${filename}.html`);
 app.use(express.static('public'))
 
-app.listen(port,() => console.log(`http://localhost:${port}`))
 
-app.get("/",(req,res) => res.sendFile(createPath("home")));
+
+app.get("/",(req,res) => res.sendFile(createPath("index")));
 
 app.get("/login",(req,res) => res.sendFile(createPath("login")));
 
@@ -17,4 +17,6 @@ app.get("/productCart",(req,res) => res.sendFile(createPath("productCart")));
 app.get("/productDetail",(req,res) => res.sendFile(createPath("productDetail")));
 
 app.get("/register",(req,res) => res.sendFile(createPath("register")));
+
+app.listen(port,() => console.log(`http://localhost:${port}`))
 
