01cd899da0dec9bdb1f594eae7e52e1d55ceae2d
Ari Kneeteman <kneetemana@gmail.com>
2024-02-24 14:26:02 -0300

    comentarios

--- controllers/admin/listProducts.controller.js
+++ controllers/admin/listProducts.controller.js
@@ -1,10 +1,13 @@
 module.exports = (req, res) => {
 
     const productos= require("../../database/productos.json")
-        
+    
+    /*Aqui solo renderizamos el contenido de la vista*/
       res.render("admin/listProducts", {productos}, (err, content) =>{
         err && res.send(err.message)
-
+    
+    // Aqui renderizamos el partials de dashboeard 
+    // QUE YA VA A TENER LA VISTA INCLUIDA QUE RENDERIZAMOS ANTERIORMENTE    
         res.render("partials/dashboard", {
           views: content
         })
