4177278786daa2def732aeb0a927404208909aa6
agustin9700 <ryuk10xd@gmail.com>
2024-03-08 21:06:05 -0300

    multer vista crear

--- middleware/uploadfile.js
+++ middleware/uploadfile.js
@@ -1,12 +1,13 @@
 const multer = require('multer');
 const path = require("path")
 
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/images/')
+      cb(null, path.join(__dirname, "../public/images"))
     },
     filename: function (req, file, cb) {
-      const formatoFile = file.fieldname  + '-' + Date.now() + path.extname(file.originalname);
+      const formatoFile = "img-"+ Date.now() + path.extname(file.originalname);
       cb(null, formatoFile)
     }
   })
