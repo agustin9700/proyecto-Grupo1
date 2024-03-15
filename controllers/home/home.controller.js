554505c97f24fb396551c8964caac0166d6098d5
Ari Kneeteman <kneetemana@gmail.com>
2024-03-14 21:56:32 -0300

    add products Home

--- controllers/home/home.controller.js
+++ controllers/home/home.controller.js
@@ -1,10 +1,11 @@
 const { loadData } = require("../../database")
 
 module.exports= (req,res) => {
-    const datos= loadData('basedate')
+    const datos= loadData('productos')
+    const datosHamb = datos.filter(p => p.category === "Hamburguesas")
     
     
-    res.render('home', {datosProductos: datos})
+    res.render('home', {datosProductos: datos, datosHamb})
 
 
 }
