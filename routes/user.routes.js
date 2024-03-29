83cae424c8dd0088c0f1ee5b83cab092f9e31308
agustin9700 <ryuk10xd@gmail.com>
2024-03-28 23:57:42 -0300

    agregando validacion en login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,11 +5,11 @@ const { upload } = require('../middleware/uploadfile')
 const userNoLogueado = require('../middleware/userNoLogeado')
 const userLogeado = require('../middleware/userLogeado')
 const validaciones = require("../middleware/Validation/registerValidation")
-
+const loginValidation = require("../middleware/Validation/loginValidation")
 
 
 router.get('/login',userNoLogueado, login)
-router.post("/login",loginProcess)
+router.post("/login",loginValidation ,loginProcess)
 
 router.get('/register',userNoLogueado, register)
 router.post('/register',[upload.single('imageProfile'),validaciones], newUser)
