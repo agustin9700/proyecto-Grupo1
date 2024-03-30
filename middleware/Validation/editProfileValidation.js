72a3961b13a151f766ef4cfa6a9df3d18ec56a35
erbaLuka7 <ezequielerba7@gmail.com>
2024-03-30 02:25:35 -0300

    continuando con las validaciones de perfil de usuario

--- middleware/Validation/editProfileValidation.js
+++ middleware/Validation/editProfileValidation.js
@@ -4,13 +4,13 @@ const path = require('path');
 const validacionesEditProfile = [
     check('name')
     .notEmpty().withMessage('El nombre es obligatorio').bail()
-    .isLength({ min: 3, max: 16 }).withMessage('El nombre debe tener entre 3 y 16 caracteres'),
+    .isLength({ min: 3, max: 26 }).withMessage('El nombre debe tener entre 3 y 26 caracteres'),
 
     check('user')
         .notEmpty().withMessage('El nombre de usuario es obligatorio'),
 
     check('phone')
-        .optional({nullable:true})
+        .optional({nullable:true, checkFalsy: true})
         .isNumeric().withMessage('debes colocar un número de teléfono'),
 
     check('email')
@@ -19,15 +19,15 @@ const validacionesEditProfile = [
         .normalizeEmail().bail(),
 
     check('province')
-        .optional({nullable:true})
+        .optional({nullable:true, checkFalsy: true})
         .isAlphanumeric('es-ES',{ignore:" "}).withMessage('Los caracteres especiales no están permitidos'),
     
     check('city')
-        .optional({nullable:true})
+        .optional({nullable:true, checkFalsy: true})
         .isAlphanumeric('es-ES',{ignore:" "}).withMessage('Los caracteres especiales no están permitidos'),
 
     check('street')
-        .optional({nullable:true})
+        .optional({nullable:true, checkFalsy: true}).bail()
         .isAlphanumeric('es-ES',{ignore:" "}).withMessage('Los caracteres especiales no están permitidos'),
 
     check('num')
@@ -35,13 +35,14 @@ const validacionesEditProfile = [
         .isNumeric().withMessage('debes colocar el número de tu localidad'),
 
     body('imageProfile')
+    .optional({nullable:true, checkFalsy: true})
         .custom((value, { req }) => {
             const file = req.file;
             const acceptExtensions = ['.jpg', '.png', '.jpeg'];
             if (!file) {
                 throw new Error('debes subir una imagen de perfil');
             } else {
-                let fileExtension = path.extname(file.originalname);
+                const fileExtension = path.extname(file.originalname);
                 if (!acceptExtensions.includes(fileExtension)) {
                     throw new Error(`Las extensiones permitidas son${acceptExtensions.join(", ")}`);
                 }
